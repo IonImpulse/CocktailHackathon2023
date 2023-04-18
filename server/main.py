7a83cabddba06ddc8444e4fbd9c1efcaed541b58
Ethan Vazquez <edv121@outlook.com>
2023-04-18 01:25:03 -0700

    change prod address

--- server/main.py
+++ server/main.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     # mount static files
     if "--prod" in sys.argv:
         # Production mode
-        uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=False,
+        uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=False,
                     workers=16, ssl_keyfile=SSL_CERT_PRIVKEY, ssl_certfile=SSL_CERT_PERMKEY)
     else:
         uvicorn.run("main:app", host="127.0.0.1", port=5000,
